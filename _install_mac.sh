7c985fcfb2ae0b6944f284db936efba2f1a3f064
Darius Pirvulescu <organicdarius@gmail.com>
2023-11-03 22:38:08 +0000

    Add the iterm2 profile config

--- _install_mac.sh
+++ _install_mac.sh
@@ -6,3 +6,5 @@ symlinkFiles=("zshrc" "aliases" "gitconfig" "gitignore" "macos" "pryrc";)
 
 trumpet "Installing tldr..."
 attempt_run brew install tldr
+
+echo "After installing iTerm2, import the 'Dario_iterm2_profile.json' into it."
