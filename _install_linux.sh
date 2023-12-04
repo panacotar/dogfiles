9268de202243c020f61e563c69409756ccc679df
Darius Pirvulescu <organicdarius@gmail.com>
2023-12-04 14:20:05 +0000

    Update script swapping CTRL <> Caps_Lock

--- _install_linux.sh
+++ _install_linux.sh
@@ -15,15 +15,22 @@ then
   echo "\n# Swaps keys ctrl-caps" >> zshrc
   echo "setxkbmap -option caps:nocaps" >> zshrc
 
+  backup "$HOME/.xmodmap"
+  symlink "$PWD/xmodmap" "$HOME/.xmodmap"
+  echo 'xmodmap ~/.xmodmap' >> zshrc
+
   # Alternative using gnome-tweaks
   # gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps']"
 fi
 
 trumpet "Installing sqlite3..."
-attempt_run sudo apt-get install sqlite3 libsqlite3-dev
+attempt_run sudo apt-get install -y sqlite3 libsqlite3-dev
 
 trumpet "Installing xclip (clipboard copy) + aliases with pbcopy..."
 attempt_run sudo apt-get install xclip
 # Emulate the pbcopy & pbpaste from Mac
 alias pbcopy='xclip -selection clipboard'
 alias pbpaste='xclip -selection clipboard -o'
+
+trumpet "Installing libavcodec-extra"
+attempt_run sudo apt install libavcodec-extra -y
