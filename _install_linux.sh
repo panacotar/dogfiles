f852f2c8f9c856c099505feb223e8124c790caf6
Darius Pirvulescu <organicdarius@gmail.com>
2024-08-21 16:54:16 -0500

    Remove generated lazygit files

--- _install_linux.sh
+++ _install_linux.sh
@@ -108,4 +108,5 @@ trumpet "Installing lazygit..."
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 attempt_run curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
-sudo install lazygit /usr/local/bin
+attempt_run sudo install lazygit /usr/local/bin
+rm lazygit.tar.gz lazygit 
